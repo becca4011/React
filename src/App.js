9725f27839cb3e5f9a5744299472e4ade0fde612
becca4011 <becca4011@naver.com>
2021-08-16 14:15:58 +0900

    state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,25 @@ import './App.css'; // App 컴포넌트의 css
 
 // App 컴포넌트를 root 태그 안에 넣음
 class App extends Component {
+  // render보다 먼저 실행, 컴포넌트를 초기화시키고 싶을 때 사용
+  constructor(props) {
+    super(props); // state 값 초기화
+    // state : 앱 내부적으로 사용할 때 state 사용 (props의 값을 바꿀 수 있음)
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
